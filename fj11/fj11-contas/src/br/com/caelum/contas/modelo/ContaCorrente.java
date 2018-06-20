9daa5a68d0209cc120208ac45a336a9e33f8ea75
Caue Queiroz <cauenqueiroz@gmail.com>
2018-06-19 22:09:38 -0300

    [fj11] Finish cap11

--- fj11/fj11-contas/src/br/com/caelum/contas/modelo/ContaCorrente.java
+++ fj11/fj11-contas/src/br/com/caelum/contas/modelo/ContaCorrente.java
@@ -1,6 +1,6 @@
 package br.com.caelum.contas.modelo;
 
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tributavel {
 	
 	@Override
 	public String getTipo() {
@@ -10,5 +10,10 @@ public class ContaCorrente extends Conta {
 	@Override
 	public void saca(double valor) {
 		this.saldo -= valor + 0.10;
+	}
+
+	@Override
+	public double getValorImposto() {
+		return this.getSaldo() * 0.01;
 	}
 }
