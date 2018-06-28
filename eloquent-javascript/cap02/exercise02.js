dc7a33a1acf5695141c8e803d73cc0cfe6886954
Caue Queiroz <cauenqueiroz@gmail.com>
2018-06-27 23:41:29 -0300

    update exercise 2 / cap 02 code

--- eloquent-javascript/cap02/exercise02.js
+++ eloquent-javascript/cap02/exercise02.js
@@ -7,18 +7,11 @@
   When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible
   by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 ===================================================================================================== */
-for (let number = 1; number <= 100; number++) {
-  let divisibleBy3 = number % 3 === 0;
-  let divisibleBy5 = number % 5 === 0;
-  let divisibleBy3And5 = divisibleBy3 && divisibleBy5;
+for (let number = 1; number <= 20; number++) {
+  let result = "";
 
-  if (divisibleBy3And5) {
-    console.log("FizzBuzz");
-  } else if (divisibleBy3) {
-    console.log("Fizz");
-  } else if (divisibleBy5) {
-    console.log("Buzz");
-  } else {
-    console.log(number);
-  }
+  if (number % 3 == 0) result += "Fizz";
+  if (number % 5 == 0) result += "Buzz";
+
+  console.log(result || number);
 }
